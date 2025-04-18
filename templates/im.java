37f2b72dfe3b7088c57868833f8a2001bbf8167a
yostane <1958676+yostane@users.noreply.github.com>
2025-04-18 11:39:34 +0200

    Some updates for devoxxuk presentation

---
+++ templates/im.java
@@ -0,0 +1,8 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 24+
+//COMPILE_OPTIONS --enable-preview -source 24
+//RUNTIME_OPTIONS --enable-preview
+
+void main(String... args) {
+    System.out.println("Hello World");
+}
